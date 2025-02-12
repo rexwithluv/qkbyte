6306ec01d2e61bd157a549309f302f71f0720fd5
rexwithluv <aaronaax@gmail.com>
2025-02-12 22:37:38 +0100

    refactor(footer): de bootstrap a MUI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,25 @@
 import Link from "next/link";
+import { Box, Typography } from "@mui/material";
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-dark text-white text-center pt-3 pb-2">
-
-            <Link href="/politica-privacidad" className="text-white">Política de privacidad</Link>
-            &nbsp;|&nbsp;
-            <Link href="/terminos" className="text-white">Términos de servicio</Link><br/>
-
-            &copy; {currentYear} QKByte - Todos los derechos reservados
-        </footer>
-    )
+        <Box
+            component="footer"
+            bgcolor="primary.main"
+            color="primary.contrastText"
+            textAlign="center"
+            py={2}
+        >
+            <Typography variant="body2">
+                <Link href="/politica-privacidad" style={{ color: 'inherit', textDecoration: 'none' }}>Política de privacidad</Link>
+                &nbsp;|&nbsp;
+                <Link href="/terminos" style={{ color: 'inherit', textDecoration: 'none' }}>Términos de servicio</Link>
+            </Typography>
+            <Typography variant="body2" mt={1}>
+                &copy; {currentYear} QKByte - Todos los derechos reservados
+            </Typography>
+        </Box>
+    );
 }
