bb3e6df5e978819138422928c0ea0672512346e9
rexwithluv <aaronaax@gmail.com>
2025-02-12 10:47:55 +0100

    feat(footer): ahora tiene la URL de contactos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
+import Link from "next/link";
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-dark text-white text-center pt-3">
-            <p>
-                <a href="/politica-privacidad" className="text-white">Política de privacidad</a>
-                &nbsp;|&nbsp;
-                <a href="/terminos" className="text-white">Términos de servicio</a> <br />
+        <footer className="bg-dark text-white text-center pt-3 pb-2">
 
-                &copy; {currentYear} QKByte - Todos los derechos reservados
-            </p>
+            <Link href="/politica-privacidad" className="text-white">Política de privacidad</Link>
+            &nbsp;|&nbsp;
+            <Link href="/terminos" className="text-white">Términos de servicio</Link><br/>
+
+            &copy; {currentYear} QKByte - Todos los derechos reservados
         </footer>
     )
 }
