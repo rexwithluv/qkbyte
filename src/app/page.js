458a5acd80c0e9802a64901466957a2826a0eafd
rexwithluv <aaronaax@gmail.com>
2025-02-12 22:43:30 +0100

    refactor: desde BootStrap hasta MUI

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,32 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import qkbyteImage from "@/assets/images/qkbyte.webp";
+import { Box, Typography, Link as MuiLink } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className={`${styles.container} d-flex flex-column align-items-center justify-content-center text-center mt-5`}>
-      <Image src={qkbyteImage} alt="Una imagen con el nombre de nuestro sitio web hecha con IA" className="mb-4" style={{ height: '300px', width: 'auto' }} />
-      <h1 className="mb-4">Bienvenida/o</h1>
-      <p className="mb-3">Te recomendamos <Link href="/login">iniciar sesión</Link> para poder comprar productos.</p>
-    </div>
+    <Box
+      className={styles.container}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+      mt={5}
+    >
+      <Image
+        src={qkbyteImage}
+        alt="Una imagen con el nombre de nuestro sitio web hecha con IA"
+        className="mb-4"
+        style={{ height: "300px", width: "auto" }}
+      />
+
+      <Typography variant="h1" component="h1" className="mb-4">
+        Bienvenida/o
+      </Typography>
+      <Typography variant="body1" className="mb-3">
+        Te recomendamos <MuiLink href="/login">iniciar sesión</MuiLink> para poder comprar productos.
+      </Typography>
+    </Box>
   );
 }
