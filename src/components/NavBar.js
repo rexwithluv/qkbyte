e96f0ebd47369ead632bf92b3c29baaa437043f6
rexwithluv <aaronaax@gmail.com>
2025-02-16 17:02:27 +0100

    refactor(navbar): cambiado desde BS a MUI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, useMediaQuery, useTheme } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Menu, Link, MenuItem, useMediaQuery, useTheme, Stack } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 
@@ -22,17 +21,14 @@ export default function BarraNavegacion() {
     };
 
     return (
-        // AppBar: Componente de barra de aplicaciones de MUI
         <AppBar position="static">
-            {/* Toolbar: Contenedor flexible para agrupar elementos */}
             <Toolbar>
-                {/* Typography: Componente para mostrar texto con estilos tipográficos */}
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Link href="/" style={{ color: 'inherit', textDecoration: 'none' }}>QKByte</Link>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    <Link href="/" color="inherit" underline="none">QKByte</Link>
                 </Typography>
+
                 {esPantallaPequena ? (
                     <>
-                        {/* IconButton: Botón para íconos, usado aquí para el menú */}
                         <IconButton
                             size="large"
                             edge="start"
@@ -41,10 +37,9 @@ export default function BarraNavegacion() {
                             sx={{ mr: 2 }}
                             onClick={manejarMenu}
                         >
-                            {/* MenuIcon: Ícono de menú de MUI */}
                             <MenuIcon />
                         </IconButton>
-                        {/* Menu: Componente de menú desplegable de MUI */}
+
                         <Menu
                             id="menu-appbar"
                             anchorEl={anclaEl}
@@ -60,28 +55,27 @@ export default function BarraNavegacion() {
                             open={Boolean(anclaEl)}
                             onClose={manejarCerrar}
                         >
-                            {/* MenuItem: Elemento de menú de MUI */}
                             <MenuItem onClick={manejarCerrar}>
-                                <Link href="#" style={{ color: 'inherit', textDecoration: 'none' }}>Hardware y periféricos</Link>
+                                <Link href="#" color="inherit" underline="none">Hardware y periféricos</Link>
                             </MenuItem>
                             <MenuItem onClick={manejarCerrar}>
-                                <Link href="#" style={{ color: 'inherit', textDecoration: 'none' }}>Software</Link>
+                                <Link href="#" color="inherit" underline="none" >Software</Link>
                             </MenuItem>
                             <MenuItem onClick={manejarCerrar}>
-                                <Link href="#" style={{ color: 'inherit', textDecoration: 'none' }}>PCs montados</Link>
+                                <Link href="#" color="inherit" underline="none" >PCs montados</Link>
                             </MenuItem>
                             <MenuItem onClick={manejarCerrar}>
-                                <Link href="/contacto" style={{ color: 'inherit', textDecoration: 'none' }}>Contacto</Link>
+                                <Link href="/contacto" color="inherit" underline="none" >Contacto</Link>
                             </MenuItem>
                         </Menu>
                     </>
                 ) : (
-                    <>
-                        <Link href="#" style={{ color: 'inherit', textDecoration: 'none', marginRight: '1rem' }}>Hardware y periféricos</Link>
-                        <Link href="#" style={{ color: 'inherit', textDecoration: 'none', marginRight: '1rem' }}>Software</Link>
-                        <Link href="#" style={{ color: 'inherit', textDecoration: 'none', marginRight: '1rem' }}>PCs montados</Link>
-                        <Link href="/contacto" style={{ color: 'inherit', textDecoration: 'none' }}>Contacto</Link>
-                    </>
+                    <Stack direction="row" spacing={2}>
+                        <Link href="#" color="inherit" underline="none" >Hardware y periféricos</Link>
+                        <Link href="#" color="inherit" underline="none" >Software</Link>
+                        <Link href="#" color="inherit" underline="none" >PCs montados</Link>
+                        <Link href="/contacto" color="inherit" underline="none">Contacto</Link>
+                    </Stack>
                 )}
             </Toolbar>
         </AppBar>
