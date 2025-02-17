e0b80c4fed0f0bd1df8f0483d6549e6239b4c3f8
rexwithluv <aaronaax@gmail.com>
2025-02-17 20:52:15 +0100

    style(navbar): cambiado desde bs estándar a react-bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,76 +1,40 @@
 "use client";
 
-import { AppBar, Link, Toolbar, IconButton, Typography, Menu, MenuItem, useMediaQuery, useTheme, Stack } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { useState } from "react";
+import Link from 'next/link';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function BarraNavegacion() {
-    // Todas estas variables y funciones son para manejar el menú desplegable
-    const tema = useTheme();
-    const esPantallaPequena = useMediaQuery(tema.breakpoints.down('md'));
-
-    const [anclaEl, establecerAnclaEl] = useState(null);
-    const abrirMenu = (ev) => establecerAnclaEl(ev.currentTarget);
-    const cerrarMenu = () => establecerAnclaEl(null);
-
     return (
-        <AppBar position="static" sx={{ backgroundColor: 'darkblue' }}>
+        <Navbar bg="primary" variant="dark" expand="lg" className="py-3">
 
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Link href="/" style={{ color: 'inherit', textDecoration: 'none' }}>QKByte</Link>
-                </Typography>
-                {esPantallaPequena ? (
-                    <>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                            onClick={abrirMenu}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anclaEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anclaEl)}
-                            onClose={cerrarMenu}
-                        >
-                            {/* MenuItem: Elemento de menú de MUI */}
-                            <MenuItem onClick={cerrarMenu}>
-                                <Link href="#" underline="none" color="inherit">Hardware y periféricos</Link>
-                            </MenuItem>
-                            <MenuItem onClick={cerrarMenu}>
-                                <Link href="#" underline="none" color="inherit">Software</Link>
-                            </MenuItem>
-                            <MenuItem onClick={cerrarMenu}>
-                                <Link href="#" underline="none" color="inherit">PCs montados</Link>
-                            </MenuItem>
-                            <MenuItem onClick={cerrarMenu}>
-                                <Link href="/contacto" underline="none" color="inherit">Contacto</Link>
-                            </MenuItem>
-                        </Menu>
-                    </>
-                ) : (
-                    <Stack spacing={2} direction={"row"}>
-                        <Link href="#" underline="none" color="inherit" >Hardware y periféricos</Link>
-                        <Link href="#" underline="none" color="inherit" >Software</Link>
-                        <Link href="#" underline="none" color="inherit" >PCs montados</Link>
-                        <Link href="/contacto" underline="none" color="inherit">Contacto</Link>
-                    </Stack>
-                )}
-            </Toolbar>
-        </AppBar>
+            <Container>
+                <Link href="/" passHref className='text-decoration-none'>
+                    <Navbar.Brand className="me-5">
+                        QKByte
+                    </Navbar.Brand>
+                </Link>
+
+                {/* El desplegable */}
+                <Navbar.Toggle aria-controls="navbarSupportedContent" className="ms-auto" />
+
+                <Navbar.Collapse id="navbarSupportedContent">
+                    <Nav className="me-auto mb-2 mb-lg-0">
+                        <Link href="/hardware-perifericos" passHref>
+                            <Nav.Link>Hardware y periféricos</Nav.Link>
+                        </Link>
+                        <Link href="/software" passHref>
+                            <Nav.Link>Software</Nav.Link>
+                        </Link>
+                        <Link href="/pcs-montados" passHref>
+                            <Nav.Link>PCs montados</Nav.Link>
+                        </Link>
+                        <Link href="/contacto" passHref>
+                            <Nav.Link>Contacto</Nav.Link>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+
+        </Navbar>
     );
 }
