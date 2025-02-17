cd881ea59509e6d84b6743ecd4a825e1864d6363
rexwithluv <aaronaax@gmail.com>
2025-02-17 20:53:26 +0100

    style: pasa de usar bootstrap estándar a usar react-bootstrap

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,31 @@
-import { Box, Typography, Link } from "@mui/material";
+import Link from 'next/link';
+import Image from 'next/image';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <Box
-      component={"div"}
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-      height="80vh"
-    >
-      <Box
-        component="img"
-        src="/images/qkbyte.png"
-        alt="Una imagen con el nombre de nuestro sitio web hecha con IA"
-        height="350px" width="auto"
-      />
-
-      <Typography variant="h3" >
-        Bienvenida/o
-      </Typography>
-      <Typography variant="subtitle1" >
-        Te recomendamos <Link href="/login">iniciar sesión</Link> para poder comprar productos.
-      </Typography>
-    </Box>
+    <Container className='d-flex flex-column align-items-center justify-content-center' style={{ height: "80vh" }}>
+      <Row>
+        <Col className='text-center'>
+          <Image
+            src="/images/qkbyte.png"
+            alt="Una imagen con el nombre de nuestro sitio web hecha con IA"
+            width={700}
+            height={400}
+            style={{ objectFit: "contain" }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='text-center'>
+          <h3 className='mt-3'>
+            Bienvenida/o
+          </h3>
+          <p>
+            Te recomendamos <Link href="/login">iniciar sesión</Link> para poder comprar productos.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
