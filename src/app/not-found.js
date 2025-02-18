1b4a8a9add32c754034f09a7e1ffdaadb2c0e519
rexwithluv <aaronaax@gmail.com>
2025-02-18 14:13:27 +0100

    refactor: pagina de error ahora con bootstrap styles

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NotFound() {
     return (
-        <div >
+        <div className="d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "75vh" }}>
+            <Image src="/images/not-found.png" alt="Not Found" style={{ objectFit: "contain" }} width={500} height={350} />
             <h1>404 - Página no encontrada</h1>
             <p>Lo sentimos, la página que buscas no existe.</p>
             <Link href="/">Volver al inicio</Link>
         </div>
     )
-
 }
