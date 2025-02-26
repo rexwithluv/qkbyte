43868111a45314ce37a42daf1670ad6eabd19e77
rexwithluv <aaronaax@gmail.com>
2025-02-26 22:58:48 +0100

    none

--- src/app/carrito/page.js
+++ src/app/carrito/page.js
@@ -7,26 +7,28 @@ const CarritoPage = () => {
     const { carrito } = useContext(CartContext);
 
     return (
-        <div>
-            <h1>Carrito de Compras</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Cantidad</th>
-                        <th>Precio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {carrito.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.productName}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.price}</td>
+        <div className="container">
+            <h1 className="text-center">Carrito de Compras</h1>
+            <div className="table-responsive">
+                <table className="table table-bordered table-hover mx-auto">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {carrito.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.productName}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
